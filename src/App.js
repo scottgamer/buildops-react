4cf7c0d5383a496133183123f1cb1520b4754dc5
Richard Munoz <richosojason@msn.com>
2020-01-06 00:00:43 -0500

    changed app.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,53 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import AuthPage from "./pages/Auth";
-import BookingsPage from "./pages/Bookings";
-// import EventsPage from './pages/Events';
 import EmployeesPage from "./pages/Employees/Employees";
 import MainNavigation from "./components/Navigation/MainNavigation";
 import AuthContext from "./context/auth-context";
 
 import "./App.css";
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [auth, setAuth] = useState({
+    userId: null,
     token: null,
-    userId: null
+    tokenExpiration: null
+  });
+
+  const login = (token, userId, tokenExpiration) => {
+    setAuth({ token: token, userId: userId });
   };
 
-  login = (token, userId, tokenExpiration) => {
-    this.setState({ token: token, userId: userId });
+  const logout = () => {
+    setAuth({ token: null, userId: null });
   };
 
-  logout = () => {
-    this.setState({ token: null, userId: null });
-  };
-
-  render() {
-    return (
-      <BrowserRouter>
-        <React.Fragment>
-          <AuthContext.Provider
-            value={{
-              token: this.state.token,
-              userId: this.state.userId,
-              login: this.login,
-              logout: this.logout
-            }}
-          >
-            <MainNavigation />
-            <main className="main-content">
-              <Switch>
-                {this.state.token && <Redirect from="/" to="/events" exact />}
-                {/* {this.state.token && (
-                  <Redirect from="/auth" to="/events" exact />
-                )} */}
-                {this.state.token && (
-                  <Redirect from="/auth" to="/employees" exact />
-                )}
-                {!this.state.token && (
-                  <Route path="/auth" component={AuthPage} />
-                )}
-                {/* <Route path="/events" component={EventsPage} /> */}
-                <Route path="/employees" component={EmployeesPage} />
-                {this.state.token && (
-                  <Route path="/bookings" component={BookingsPage} />
-                )}
-                {!this.state.token && <Redirect to="/auth" exact />}
-              </Switch>
-            </main>
-          </AuthContext.Provider>
-        </React.Fragment>
-      </BrowserRouter>
-    );
-  }
-}
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <AuthContext.Provider
+          value={{
+            token: auth.token,
+            userId: auth.userId,
+            login: login,
+            logout: logout
+          }}
+        >
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              {auth.token && <Redirect from="/" to="/employees" exact />}
+              {auth.token && <Redirect from="/auth" to="/employees" exact />}
+              {!auth.token && <Route path="/auth" component={AuthPage} />}
+              <Route path="/employees" component={EmployeesPage} />
+              {!auth.token && <Redirect to="/auth" exact />}
+            </Switch>
+          </main>
+        </AuthContext.Provider>
+      </React.Fragment>
+    </BrowserRouter>
+  );
+};
 
 export default App;
